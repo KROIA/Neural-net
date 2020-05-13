79c91238592fdb05ee99ad97511b2c8f43a88c07
Saurausli <51965622+Saurausli@users.noreply.github.com>
2020-05-13 11:20:48 +0200

    Release

---
+++ NnqmlUi/build-Nnqmlui/moc_mainwindow.cpp
@@ -0,0 +1,509 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Mainwindow_t {
+    QByteArrayData data[45];
+    char stringdata0[552];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Mainwindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Mainwindow_t qt_meta_stringdata_Mainwindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "Mainwindow"
+QT_MOC_LITERAL(1, 11, 19), // "averageErrorChanged"
+QT_MOC_LITERAL(2, 31, 0), // ""
+QT_MOC_LITERAL(3, 32, 20), // "learningStepsChanged"
+QT_MOC_LITERAL(4, 53, 14), // "hiddenXChanged"
+QT_MOC_LITERAL(5, 68, 14), // "hiddenYChanged"
+QT_MOC_LITERAL(6, 83, 12), // "inputChanged"
+QT_MOC_LITERAL(7, 96, 13), // "outputChanged"
+QT_MOC_LITERAL(8, 110, 11), // "biasChanged"
+QT_MOC_LITERAL(9, 122, 18), // "trainingSetChanged"
+QT_MOC_LITERAL(10, 141, 23), // "maxLearningStepsChanged"
+QT_MOC_LITERAL(11, 165, 16), // "outputSetChanged"
+QT_MOC_LITERAL(12, 182, 17), // "errorChartChanged"
+QT_MOC_LITERAL(13, 200, 18), // "netStructurChanged"
+QT_MOC_LITERAL(14, 219, 15), // "netValueChanged"
+QT_MOC_LITERAL(15, 235, 5), // "start"
+QT_MOC_LITERAL(16, 241, 4), // "stop"
+QT_MOC_LITERAL(17, 246, 8), // "uiUpdate"
+QT_MOC_LITERAL(18, 255, 5), // "reset"
+QT_MOC_LITERAL(19, 261, 8), // "creatNew"
+QT_MOC_LITERAL(20, 270, 8), // "maxError"
+QT_MOC_LITERAL(21, 279, 8), // "maxSteps"
+QT_MOC_LITERAL(22, 288, 9), // "addNeuron"
+QT_MOC_LITERAL(23, 298, 13), // "addConnection"
+QT_MOC_LITERAL(24, 312, 3), // "end"
+QT_MOC_LITERAL(25, 316, 7), // "hiddenX"
+QT_MOC_LITERAL(26, 324, 7), // "hiddenY"
+QT_MOC_LITERAL(27, 332, 6), // "output"
+QT_MOC_LITERAL(28, 339, 5), // "input"
+QT_MOC_LITERAL(29, 345, 12), // "averageError"
+QT_MOC_LITERAL(30, 358, 13), // "learningSteps"
+QT_MOC_LITERAL(31, 372, 16), // "maxLearningSteps"
+QT_MOC_LITERAL(32, 389, 4), // "bias"
+QT_MOC_LITERAL(33, 394, 11), // "trainingSet"
+QT_MOC_LITERAL(34, 406, 18), // "std::vector<qreal>"
+QT_MOC_LITERAL(35, 425, 13), // "trainingSetId"
+QT_MOC_LITERAL(36, 439, 9), // "outputSet"
+QT_MOC_LITERAL(37, 449, 10), // "errorChart"
+QT_MOC_LITERAL(38, 460, 11), // "startNeuron"
+QT_MOC_LITERAL(39, 472, 16), // "std::vector<int>"
+QT_MOC_LITERAL(40, 489, 9), // "endNeuron"
+QT_MOC_LITERAL(41, 499, 9), // "neuronTyp"
+QT_MOC_LITERAL(42, 509, 16), // "connectionWeight"
+QT_MOC_LITERAL(43, 526, 15), // "neuronValueVect"
+QT_MOC_LITERAL(44, 542, 9) // "activFunc"
+
+    },
+    "Mainwindow\0averageErrorChanged\0\0"
+    "learningStepsChanged\0hiddenXChanged\0"
+    "hiddenYChanged\0inputChanged\0outputChanged\0"
+    "biasChanged\0trainingSetChanged\0"
+    "maxLearningStepsChanged\0outputSetChanged\0"
+    "errorChartChanged\0netStructurChanged\0"
+    "netValueChanged\0start\0stop\0uiUpdate\0"
+    "reset\0creatNew\0maxError\0maxSteps\0"
+    "addNeuron\0addConnection\0end\0hiddenX\0"
+    "hiddenY\0output\0input\0averageError\0"
+    "learningSteps\0maxLearningSteps\0bias\0"
+    "trainingSet\0std::vector<qreal>\0"
+    "trainingSetId\0outputSet\0errorChart\0"
+    "startNeuron\0std::vector<int>\0endNeuron\0"
+    "neuronTyp\0connectionWeight\0neuronValueVect\0"
+    "activFunc"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Mainwindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      20,   14, // methods
+      18,  142, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+      13,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  114,    2, 0x06 /* Public */,
+       3,    0,  115,    2, 0x06 /* Public */,
+       4,    0,  116,    2, 0x06 /* Public */,
+       5,    0,  117,    2, 0x06 /* Public */,
+       6,    0,  118,    2, 0x06 /* Public */,
+       7,    0,  119,    2, 0x06 /* Public */,
+       8,    0,  120,    2, 0x06 /* Public */,
+       9,    0,  121,    2, 0x06 /* Public */,
+      10,    0,  122,    2, 0x06 /* Public */,
+      11,    0,  123,    2, 0x06 /* Public */,
+      12,    0,  124,    2, 0x06 /* Public */,
+      13,    0,  125,    2, 0x06 /* Public */,
+      14,    0,  126,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+      15,    0,  127,    2, 0x0a /* Public */,
+      16,    0,  128,    2, 0x0a /* Public */,
+      17,    0,  129,    2, 0x0a /* Public */,
+      18,    0,  130,    2, 0x0a /* Public */,
+      19,    2,  131,    2, 0x0a /* Public */,
+      22,    0,  136,    2, 0x0a /* Public */,
+      23,    2,  137,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Float, QMetaType::Int,   20,   21,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::UInt, QMetaType::UInt,   15,   24,
+
+ // properties: name, type, flags
+      25, QMetaType::Int, 0x00495103,
+      26, QMetaType::Int, 0x00495103,
+      27, QMetaType::Int, 0x00495103,
+      28, QMetaType::Int, 0x00495103,
+      29, QMetaType::Float, 0x00495001,
+      30, QMetaType::Int, 0x00495001,
+      31, QMetaType::Int, 0x00495103,
+      32, QMetaType::Bool, 0x00495103,
+      33, 0x80000000 | 34, 0x0049510b,
+      35, QMetaType::Int, 0x00095102,
+      36, 0x80000000 | 34, 0x0049510b,
+      37, 0x80000000 | 34, 0x00495009,
+      38, 0x80000000 | 39, 0x00495009,
+      40, 0x80000000 | 39, 0x00495009,
+      41, 0x80000000 | 39, 0x00495009,
+      42, 0x80000000 | 34, 0x00495009,
+      43, 0x80000000 | 34, 0x00495009,
+      44, QMetaType::Int, 0x00495103,
+
+ // properties: notify_signal_id
+       2,
+       3,
+       5,
+       4,
+       0,
+       1,
+       8,
+       6,
+       7,
+       0,
+       9,
+      10,
+      11,
+      11,
+      11,
+      12,
+      12,
+      11,
+
+       0        // eod
+};
+
+void Mainwindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Mainwindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->averageErrorChanged(); break;
+        case 1: _t->learningStepsChanged(); break;
+        case 2: _t->hiddenXChanged(); break;
+        case 3: _t->hiddenYChanged(); break;
+        case 4: _t->inputChanged(); break;
+        case 5: _t->outputChanged(); break;
+        case 6: _t->biasChanged(); break;
+        case 7: _t->trainingSetChanged(); break;
+        case 8: _t->maxLearningStepsChanged(); break;
+        case 9: _t->outputSetChanged(); break;
+        case 10: _t->errorChartChanged(); break;
+        case 11: _t->netStructurChanged(); break;
+        case 12: _t->netValueChanged(); break;
+        case 13: _t->start(); break;
+        case 14: _t->stop(); break;
+        case 15: _t->uiUpdate(); break;
+        case 16: _t->reset(); break;
+        case 17: _t->creatNew((*reinterpret_cast< float(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 18: _t->addNeuron(); break;
+        case 19: _t->addConnection((*reinterpret_cast< uint(*)>(_a[1])),(*reinterpret_cast< uint(*)>(_a[2]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::averageErrorChanged)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::learningStepsChanged)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::hiddenXChanged)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::hiddenYChanged)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::inputChanged)) {
+                *result = 4;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::outputChanged)) {
+                *result = 5;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::biasChanged)) {
+                *result = 6;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::trainingSetChanged)) {
+                *result = 7;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::maxLearningStepsChanged)) {
+                *result = 8;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::outputSetChanged)) {
+                *result = 9;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::errorChartChanged)) {
+                *result = 10;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::netStructurChanged)) {
+                *result = 11;
+                return;
+            }
+        }
+        {
+            using _t = void (Mainwindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Mainwindow::netValueChanged)) {
+                *result = 12;
+                return;
+            }
+        }
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<Mainwindow *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< int*>(_v) = _t->hiddenX(); break;
+        case 1: *reinterpret_cast< int*>(_v) = _t->hiddenY(); break;
+        case 2: *reinterpret_cast< int*>(_v) = _t->output(); break;
+        case 3: *reinterpret_cast< int*>(_v) = _t->input(); break;
+        case 4: *reinterpret_cast< float*>(_v) = _t->averageError(); break;
+        case 5: *reinterpret_cast< int*>(_v) = _t->learningSteps(); break;
+        case 6: *reinterpret_cast< int*>(_v) = _t->maxLearningSteps(); break;
+        case 7: *reinterpret_cast< bool*>(_v) = _t->bias(); break;
+        case 8: *reinterpret_cast< std::vector<qreal>*>(_v) = _t->trainingSet(); break;
+        case 10: *reinterpret_cast< std::vector<qreal>*>(_v) = _t->outputSet(); break;
+        case 11: *reinterpret_cast< std::vector<qreal>*>(_v) = _t->errorChart(); break;
+        case 12: *reinterpret_cast< std::vector<int>*>(_v) = _t->startNeuron(); break;
+        case 13: *reinterpret_cast< std::vector<int>*>(_v) = _t->endNeuron(); break;
+        case 14: *reinterpret_cast< std::vector<int>*>(_v) = _t->neuronTyp(); break;
+        case 15: *reinterpret_cast< std::vector<qreal>*>(_v) = _t->connectionWeight(); break;
+        case 16: *reinterpret_cast< std::vector<qreal>*>(_v) = _t->neuronValueVect(); break;
+        case 17: *reinterpret_cast< int*>(_v) = _t->activFunc(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<Mainwindow *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: _t->setHiddenX(*reinterpret_cast< int*>(_v)); break;
+        case 1: _t->setHiddenY(*reinterpret_cast< int*>(_v)); break;
+        case 2: _t->setOutput(*reinterpret_cast< int*>(_v)); break;
+        case 3: _t->setInput(*reinterpret_cast< int*>(_v)); break;
+        case 6: _t->setMaxLearningSteps(*reinterpret_cast< int*>(_v)); break;
+        case 7: _t->setBias(*reinterpret_cast< bool*>(_v)); break;
+        case 8: _t->setTrainingSet(*reinterpret_cast< std::vector<qreal>*>(_v)); break;
+        case 9: _t->setTrainingSetId(*reinterpret_cast< int*>(_v)); break;
+        case 10: _t->setOutputSet(*reinterpret_cast< std::vector<qreal>*>(_v)); break;
+        case 17: _t->setActivFunc(*reinterpret_cast< int*>(_v)); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    }
+#endif // QT_NO_PROPERTIES
+}
+
+QT_INIT_METAOBJECT const QMetaObject Mainwindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_Mainwindow.data,
+    qt_meta_data_Mainwindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Mainwindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Mainwindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Mainwindow.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Mainwindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 20)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 20;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 20)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 20;
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 18;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 18;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 18;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 18;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 18;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
+
+// SIGNAL 0
+void Mainwindow::averageErrorChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void Mainwindow::learningStepsChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void Mainwindow::hiddenXChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void Mainwindow::hiddenYChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+
+// SIGNAL 4
+void Mainwindow::inputChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 4, nullptr);
+}
+
+// SIGNAL 5
+void Mainwindow::outputChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 5, nullptr);
+}
+
+// SIGNAL 6
+void Mainwindow::biasChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 6, nullptr);
+}
+
+// SIGNAL 7
+void Mainwindow::trainingSetChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 7, nullptr);
+}
+
+// SIGNAL 8
+void Mainwindow::maxLearningStepsChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 8, nullptr);
+}
+
+// SIGNAL 9
+void Mainwindow::outputSetChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 9, nullptr);
+}
+
+// SIGNAL 10
+void Mainwindow::errorChartChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 10, nullptr);
+}
+
+// SIGNAL 11
+void Mainwindow::netStructurChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 11, nullptr);
+}
+
+// SIGNAL 12
+void Mainwindow::netValueChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 12, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
